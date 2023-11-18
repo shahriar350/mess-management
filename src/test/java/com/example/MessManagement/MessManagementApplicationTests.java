25b81869d094e79ae1e7f9013ee89d43708d63a8
saifullah-repliq <shahen.saifullah@repliq.dev>
2023-11-18 21:25:53 +0600

    This is a new commit for what I originally planned to be amended

---
+++ src/test/java/com/example/MessManagement/MessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MessManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
