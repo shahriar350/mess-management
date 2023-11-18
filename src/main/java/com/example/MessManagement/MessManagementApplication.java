55acbec9bd2dc62b984a46c41aa6b51a201a8842
monzurulislam <iammonzurulislam@gmail.com>
2023-11-18 23:27:00 +0600

    First Commit.

--- src/main/java/com/example/MessManagement/MessManagementApplication.java
+++ src/main/java/com/example/MessManagement/MessManagementApplication.java
@@ -9,5 +9,5 @@ public class MessManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MessManagementApplication.class, args);
 	}
-
+	System.out.println("Hello Saifullah!");
 }
